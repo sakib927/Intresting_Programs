2ebcd6fbf32d4cdfa7851e76cd7187b7815e96b3
sakib927 <82685261+sakib927@users.noreply.github.com>
2023-02-17 18:10:52 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,7 @@ import random
 
 possibleNumbers = [1,2,3,4,5,6,7,8,9]
 gameBoard = [[1,2,3], [4,5,6], [7,8,9]]
-
+rows = 3
 def printGameBoard():
   for x in range(rows):
       print(gameBoard[x],end = '\n')
